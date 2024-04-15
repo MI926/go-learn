995663afdc5539d57390e800abfae933f88a475d
MI926 <51293372+MI926@users.noreply.github.com>
2024-04-15 17:40:03 +0000

    just print statment

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+}
+//this is the way to comment single line
+/*this is the way to comment multiple lines
+*this how to
