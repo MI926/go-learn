3efd7d920d43c7afcab22c07f04d3dc0ec732770
MI926 <51293372+MI926@users.noreply.github.com>
2024-04-26 12:56:09 +0000

    add something

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ func main() {
 }
 //this is the way to comment single line
 /*this is the way to comment multiple lines
-//this how to
+*this how to*/
 package main
 
 import "fmt"
 
 func main() {
-
+    /*
     fmt.Println("go" + "lang")//This just like adding two string and give golang
 
     fmt.Println("1+1 =", 1+1)//This is just add two number which are not in doble inverted comma give 1 + 1 = 2
@@ -24,6 +24,22 @@ func main() {
     fmt.Println(true && false)//here && means and so true and false is false
     fmt.Println(true || false)//here || is or so true or false is true
     fmt.Println(!true)// here ! is use for not
+    */
+    //variable declaration
+    var a = "mani"
+    var b = "gupta"
+    var c, d = 15, 25 //it's in string not in integer
+    var e, f int = 245, 265 //here it's in integer
+    fmt.Println(e + f)
+    fmt.Println(e, f)
+    var g = true //here true is in boolean
+    var h int //If we don't determine the value then it take 0
+    i := "apple"//this is also a way to define the variable
+    fmt.Println(f)
+    fmt.Println(a)
+    fmt.Println(b)
+    fmt.Println(c + d)
+    fmt.Println(g)
+    fmt.Println(h)
+    fmt.Println(i)
 }
-
-*/
