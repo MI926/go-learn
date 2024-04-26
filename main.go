3736e74b6ff67cc3b673d4f98822e590047f6dd3
MI926 <51293372+MI926@users.noreply.github.com>
2024-04-26 12:35:37 +0000

    add somecode

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import "fmt"
 
@@ -7,4 +7,23 @@ func main() {
 }
 //this is the way to comment single line
 /*this is the way to comment multiple lines
-*this how to
+//this how to
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("go" + "lang")//This just like adding two string and give golang
+
+    fmt.Println("1+1 =", 1+1)//This is just add two number which are not in doble inverted comma give 1 + 1 = 2
+    //1 + 1 is in string
+    fmt.Println("7.0/3.0 =", 7.0/3.0) // here 7.0/ 3.0 = is in string and 7.0/3.0 in integer so int operations applicable
+ 
+
+    fmt.Println(true && false)//here && means and so true and false is false
+    fmt.Println(true || false)//here || is or so true or false is true
+    fmt.Println(!true)// here ! is use for not
+}
+
+*/
